c4b2504100a7ac0c68011c1d629f2043a4db9ed2
softsamiul <softsamiul@gmail.com>
2021-10-06 16:11:22 +0530

    react router added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom";
 import './App.css';
 import Header from './components/Header/Header';
+import Inventory from "./components/Inventory/Inventory";
+import NotFound from "./components/NotFound/NotFound";
+import OrderReview from "./components/OrderReview/OrderReview";
 import Shop from './components/Shop/Shop';
-
 function App() {
   return (
     <div>
-      <Header></Header>
-      <Shop></Shop>
+      
+      
+
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Shop></Shop>
+          </Route>
+          <Route exact path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route path="/inventory">
+            <Inventory></Inventory>
+          </Route>
+          <Route path="/review">
+            <OrderReview></OrderReview>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
